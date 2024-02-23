9f651f2f3a5406c52db6f48c8cb3b5f190b15927
tobby340602 <niklaussergei23@gmail.com>
2024-02-23 11:50:17 +1000

    second commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      className="App"
+      style={{
+        position: "fixed",
+        top: 0,
+        left: 0,
+        width: "100%",
+        height: "100vh",
+      }}>
+      <iframe
+        title="Mystery-game-frame"
+        src="https://i.simmer.io/@Tobby340602/mystery-game"
+        style={{ width: "100%", height: "100%", border: "none" }}></iframe>
     </div>
   );
 }
